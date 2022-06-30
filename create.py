ef5b6b95950b006b836b297f26c84cf39d4680ac
laportag <greglaporta@hotmail.com>
2022-06-30 09:31:37 +0000

    commented out  examples from create.py

--- create.py
+++ create.py
@@ -4,19 +4,19 @@ from application.models import Plants, Gardens, Pla_Gar
 db.drop_all()
 db.create_all()
 
-sample_garden = Gardens(
-    address = "Sample Street, Sampleton, SMP3L3"
-)
-sample_plant = Plants(
-    com_name = "Sample",
-    sci_name = "Exemplum planta"
-)
-sample_pla_gar = Pla_Gar(
-    plant_id = "1",
-    garden_id = "1"
-)
-db.session.add(sample_garden)
-db.session.add(sample_plant)
-db.session.add(sample_pla_gar)
+# sample_garden = Gardens(
+#     address = "Sample Street, Sampleton, SMP3L3"
+# )
+# sample_plant = Plants(
+#     com_name = "Sample",
+#     sci_name = "Exemplum planta"
+# )
+# sample_pla_gar = Pla_Gar(
+#     plant_id = "1",
+#     garden_id = "1"
+# )
+# db.session.add(sample_garden)
+# db.session.add(sample_plant)
+# db.session.add(sample_pla_gar)
 
-db.session.commit()
+# db.session.commit()
